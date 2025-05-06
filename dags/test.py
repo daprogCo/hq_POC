999f51fb90e6b854a8d478ba7223406bcd4b1391
root <root@srv766682.hstgr.cloud>
2025-05-06 02:50:03 +0000

    sans spark

--- dags/test.py
+++ dags/test.py
@@ -2,7 +2,6 @@ from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 from plugins.get_pannes_data import main as get_pannes_data
-from plugins.extract_data import main as extract_data
 from datetime import timedelta
 
 
@@ -12,7 +11,7 @@ args = {
 }
 
 dag = DAG(
-    dag_id='get_hydroquebec_data',
+    dag_id='fetch_hq_data',
     default_args=args,
     schedule_interval=timedelta(minutes=10),
     catchup=False
@@ -23,10 +22,5 @@ with dag:
         task_id='run_task_1',
         python_callable=get_pannes_data
     )
-    run_task_2 = PythonOperator(
-        task_id='run_task_2',
-        python_callable=extract_data,
-        provide_context=True
-    )
 
-    run_task_1 >> run_task_2
+    run_task_1
